4a0d44bbc5cfb1785c3f154c28123347f0d7c526
Juan John-Charles <whoisju1@gmail.com>
2018-05-19 19:51:54 -0400

    setup resolver function for adding Options

--- resolvers.js
+++ resolvers.js
@@ -25,7 +25,13 @@ const Mutation = {
     }
   },
   createOptions: async (root, { input }) => null,
-  addOption: async (root, { input }) => null,
+  addOption: async (root, { input }, { postgres }) => {
+    const [option] = await postgres('option')
+      .insert(input)
+      .returning(['answer', 'option_id', 'question_id', { isCorrect: 'is_correct' }])
+      .from('option');
+    return option;
+  },
 };
 
 const Quiz = {
