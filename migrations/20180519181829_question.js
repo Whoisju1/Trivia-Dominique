906448f45b05fd638915e977787837ad49848fec
Juan John-Charles <whoisju1@gmail.com>
2018-05-19 18:23:49 -0400

    create migration file for question table

---
+++ migrations/20180519181829_question.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('question', function(table) {
+    table.increments('question_id').notNullable();
+    table.integer('quiz_id').references('quiz.quiz_id').notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('question');
+};
