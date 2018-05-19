947ebdcf09670a1eb3fc24ec29c3bf5e26ea586e
Juan John-Charles <whoisju1@gmail.com>
2018-05-19 19:16:13 -0400

    add fields and constraints to question table

--- migrations/20180519181829_question.js
+++ migrations/20180519181829_question.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('question', function(table) {
     table.increments('question_id').notNullable();
+    table.string('question').notNullable().unique();
     table.integer('quiz_id').references('quiz.quiz_id').notNullable();
   });
 };
