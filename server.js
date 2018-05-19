555c406aa3d720fc2c860b5347a28d77a4b91d86
Juan John-Charles <whoisju1@gmail.com>
2018-05-19 16:42:22 -0400

    set up graphql in server

--- server.js
+++ server.js
@@ -2,10 +2,30 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+const fs = require('fs');
+const resolvers = require('./resolvers');
+
+const typeDefs = fs.readFileSync('./schema.gql', { encoding: 'utf8' });
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const app = express();
-const PORT = process.env;
+const { PORT } = process.env;
 
 app.use(bodyParser.json());
+
+app.use('/graphql', graphqlExpress(req => ({
+  schema,
+  context: {
+    req,
+  },
+})));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
