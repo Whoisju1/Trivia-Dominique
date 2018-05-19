7f5a442138ef4d3079fb417b5ba523896665e876
Juan John-Charles <whoisju1@gmail.com>
2018-05-19 17:42:45 -0400

    make changes in knexfile connection object

--- knexfile.js
+++ knexfile.js
@@ -6,8 +6,8 @@ module.exports = {
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      user : process.env.PG_USER,
-      password: process.env.PG_PASS,
+      user : 'postgres',
+      password: 'root',
       database : 'trivia_dominique'
     }
   },
