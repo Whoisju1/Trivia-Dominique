a47cd0b7a1f89e07edb36412c2d8ed7d515cd3de
Juan John-Charles <whoisju1@gmail.com>
2018-05-20 07:24:21 -0400

    install polished and styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    country: 'Dominique',
+  }
+
   render() {
+    const { country } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Trivia { country }
       </div>
     );
   }
